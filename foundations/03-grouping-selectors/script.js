99af7da01d98a3ff2ab635c4232489ddaa1db124
Anna Saratovskaia <annja@list.ru>
2022-10-04 17:38:03 -0700

    My solution for CSS exercises

--- foundations/03-grouping-selectors/script.js
+++ foundations/03-grouping-selectors/script.js
@@ -1,2 +1,14 @@
 'strict';
-console.log('aaa');
+console.log('a' > 'aac');
+
+const arr = [1, 4, 6, 134, 13];
+const arrSorted = arr.sort();
+
+console.log(arrSorted);
+
+arrSorted.sort(function (a, b) {
+  return b - a;
+});
+console.log( arrSorted);
+
+
